e16ad1c0c6b9705a133f23a4e355c22478ae96fd
Eduardo Valencia <l.alo.red@hotmail.com>
2018-09-18 20:28:36 -0500

    Update Generator.swift

--- Sources/Generator.swift
+++ Sources/Generator.swift
@@ -223,7 +223,7 @@ private extension Generator {
     static func validateDigits(_ digits: Int) throws {
         // https://tools.ietf.org/html/rfc4226#section-5.3 states "Implementations MUST extract a
         // 6-digit code at a minimum and possibly 7 and 8-digit codes."
-        let acceptableDigits = 6...8
+        let acceptableDigits = 6...20
         guard acceptableDigits.contains(digits) else {
             throw Error.invalidDigits
         }
